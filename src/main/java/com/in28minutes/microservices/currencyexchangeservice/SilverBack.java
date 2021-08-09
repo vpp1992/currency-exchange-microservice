665e90a8a2bd6351c2252699bf809ab6de6669de
Vineet Pandey <vineet1992.vp@gmail.com>
2021-08-09 16:08:24 +0530

    add new class

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/SilverBack.java
@@ -0,0 +1,11 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+public class SilverBack {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+
+    }
+}
