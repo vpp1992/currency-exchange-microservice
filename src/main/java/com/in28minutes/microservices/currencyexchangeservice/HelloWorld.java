710fcd21c008f19b1a648d469dbbe931bb62971e
Vineet Pandey <vineet1992.vp@gmail.com>
2021-08-09 14:17:29 +0530

    print messge hi

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public String sayHello(String name) {
 
         System.out.println("sayHello called with name : " + name);
-
+        System.out.println("sayHello called with : " + name);
         return "Hello " + name;
     }
 
